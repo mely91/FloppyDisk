f104722833803544dda5dc47d4874922d46466f2
Amelia Martinez <mely91@bu.edu>
2013-05-07 19:55:32 -0400

    pushing final assignment 2

---
+++ Final AS2/lib/protocol_client.c
@@ -0,0 +1,249 @@
+/******************************************************************************
+* Copyright (C) 2011 by Jonathan Appavoo, Boston University
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <strings.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "protocol.h"
+#include "protocol_utils.h"
+#include "protocol_client.h"
+
+#define FIXME
+
+typedef struct {
+  Proto_Session rpc_session;
+  Proto_Session event_session;
+  pthread_t EventHandlerTid;
+  Proto_MT_Handler session_lost_handler;
+  Proto_MT_Handler base_event_handlers[PROTO_MT_EVENT_BASE_RESERVED_LAST 
+				       - PROTO_MT_EVENT_BASE_RESERVED_FIRST
+				       - 1];
+} Proto_Client;
+
+extern Proto_Session *
+proto_client_rpc_session(Proto_Client_Handle ch)
+{ printf("proto_client_rpc_session\n");
+  Proto_Client *c = ch;
+  return &(c->rpc_session);
+}
+
+extern Proto_Session *
+proto_client_event_session(Proto_Client_Handle ch)
+{
+    printf("proto_client_event_session");
+  Proto_Client *c = ch;
+  return &(c->event_session);
+}
+
+extern int
+proto_client_set_session_lost_handler(Proto_Client_Handle ch, Proto_MT_Handler h)
+{ printf("proto_client_set_session_lost_handler\n");
+  Proto_Client *c = ch;
+  c->session_lost_handler = h;
+  return 0;
+}
+
+extern int
+proto_client_set_event_handler(Proto_Client_Handle ch, Proto_Msg_Types mt,
+			       Proto_MT_Handler h)
+{
+    printf("inside proto_client_set_event_handler\n");
+     int i;
+  Proto_Client *c = ch;
+
+  if (mt>PROTO_MT_EVENT_BASE_RESERVED_FIRST && 
+      mt<PROTO_MT_EVENT_BASE_RESERVED_LAST) {
+    i=mt - PROTO_MT_EVENT_BASE_RESERVED_FIRST - 1;
+    c->base_event_handlers[i]=h;
+      printf("succes proto_client_set_event_handler \n");
+    return 1;
+  } else {
+      printf("returned -1 inside proto_client_set_event_handler \n");
+    return -1;
+  }
+}
+
+static int 
+proto_client_session_lost_default_hdlr(Proto_Session *s)
+{
+  fprintf(stderr, "Session lost...:\n");
+  proto_session_dump(s);
+  return -1;
+}
+
+static int 
+proto_client_event_null_handler(Proto_Session *s)
+{
+  fprintf(stderr, 
+	  "proto_client_event_null_handler: invoked for session:\n");
+  proto_session_dump(s);
+
+  return 1;
+}
+
+static void *
+proto_client_event_dispatcher(void * arg)
+{printf("inside proto_clietnt_event_dispatcher");
+  Proto_Client *c;
+  Proto_Session *s;
+  Proto_Msg_Types mt;
+  Proto_MT_Handler hdlr;
+  int i;
+
+  pthread_detach(pthread_self());
+  printf("Inside pthread_detach\n");
+  c = arg;
+  s = &c->event_session;
+
+  fprintf(stderr, "About to enter infinite loop to process events:\n");
+  proto_session_dump(s);
+
+  for (;;) {
+    if (proto_session_rcv_msg(s)==1) {
+      fprintf(stderr, "Got a message on the event channel:\n");
+      proto_session_dump(s);
+
+      mt = proto_session_hdr_unmarshall_type(s);
+      if (mt > PROTO_MT_EVENT_BASE_RESERVED_FIRST && 
+	  mt < PROTO_MT_EVENT_BASE_RESERVED_LAST) {
+	i=mt - PROTO_MT_EVENT_BASE_RESERVED_FIRST - 1;
+	  hdlr=c->base_event_handlers[i];
+
+         
+	if (hdlr(s)<0) goto leave;
+      }
+    } else {
+      proto_client_session_lost_default_hdlr(s);   
+      goto leave;
+    }
+  }
+ leave:
+  
+  close(s->fd);
+  return NULL;
+}
+
+extern int
+proto_client_init(Proto_Client_Handle *ch)
+{
+  Proto_Msg_Types mt;
+  Proto_Client *c;
+ 
+  c = (Proto_Client *)malloc(sizeof(Proto_Client));
+  if (c==NULL) return -1;
+  bzero(c, sizeof(Proto_Client));
+
+  proto_client_set_session_lost_handler(c, 
+			      	proto_client_session_lost_default_hdlr);
+  
+  for (mt=PROTO_MT_EVENT_BASE_RESERVED_FIRST+1;
+       mt<PROTO_MT_EVENT_BASE_RESERVED_LAST; mt++)
+    proto_client_set_event_handler(c, mt , proto_client_event_null_handler);
+    printf("succes inside proto_client_init\n");
+    *ch = c;
+  return 1;
+}
+
+int
+proto_client_connect(Proto_Client_Handle ch, char *host, PortType port)
+{
+  Proto_Client *c = (Proto_Client *)ch;
+  printf("tryies proto_client_connect to rpc\n");
+  if (net_setup_connection(&(c->rpc_session.fd), host, port)<0) 
+    return -1;
+    printf("tryies proto_client_connect to event\n");
+
+  if (net_setup_connection(&(c->event_session.fd), host, port+1)<0) 
+    return -2; 
+
+  if (pthread_create(&(c->EventHandlerTid), NULL, 
+		     &proto_client_event_dispatcher, c) !=0) {
+    fprintf(stderr, 
+	    "proto_client_init: create EventHandler thread failed\n");
+    perror("proto_client_init:");
+    return -3;
+  }
+
+  return 0;
+}
+
+static void
+marshall_mtonly(Proto_Session *s, Proto_Msg_Types mt) {
+    printf(" inside marshall_mtonly \n");
+Proto_Msg_Hdr h;
+  bzero(&h, sizeof(h));
+  h.type = mt;
+  proto_session_hdr_marshall(s, &h);
+   printf("exits marshall_mtonly \n"); 
+};
+
+// all rpc's are assume to only reply only with a return code in the body
+// eg.  like the null_mes
+static int 
+do_generic_dummy_rpc(Proto_Client_Handle ch, Proto_Msg_Types mt)
+    { printf("inside do_generic_rpc\n");
+  int rc;
+  Proto_Session *s;
+  Proto_Client *c = ch;
+
+       
+  s = &c->rpc_session;
+  // marshall
+
+  marshall_mtonly(s, mt);
+  rc = proto_session_rpc(s);
+
+  if (rc==1) {
+    proto_session_body_unmarshall_int(s, 0, &rc);
+  } else {
+    c->session_lost_handler(s);
+    close(s->fd);
+    // fprintf(stderr, "Error: Unable to do_generic (%d)\n", errno);
+
+  }
+  
+  return rc;
+}
+extern int 
+proto_client_hello(Proto_Client_Handle ch)
+    {printf("inside proto_client_hello\n");
+  return do_generic_dummy_rpc(ch,PROTO_MT_REQ_BASE_HELLO);  
+}
+
+extern int 
+proto_client_move(Proto_Client_Handle ch, char data)
+    { printf("inside proto_client_move\n");
+  return do_generic_dummy_rpc(ch,PROTO_MT_REQ_BASE_MOVE);  
+}
+
+extern int 
+proto_client_goodbye(Proto_Client_Handle ch)
+    { printf("inside_proto_goodbye\n");
+  return do_generic_dummy_rpc(ch,PROTO_MT_REQ_BASE_GOODBYE);  
+}
+
+
---
+++ Final AS2/lib/protocol_session.c
@@ -0,0 +1,390 @@
+/******************************************************************************
+* Copyright (C) 2011 by Jonathan Appavoo, Boston University
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <strings.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include "net.h"
+#include "protocol.h"
+#include "protocol_utils.h"
+#include "protocol_session.h"
+
+#define FIXME
+
+extern void
+proto_session_dump(Proto_Session *s)
+{ printf("inside proto_session_dump\n");
+  fprintf(stderr, "Session s=%p:\n", s);
+  fprintf(stderr, " fd=%d, extra=%p slen=%d, rlen=%d\n shdr:\n  ", 
+	  s->fd, s->extra,
+	  s->slen, s->rlen);
+  proto_dump_msghdr(&(s->shdr));
+  fprintf(stderr, " rhdr:\n  ");
+  proto_dump_msghdr(&(s->rhdr));
+}
+
+extern void
+proto_session_init(Proto_Session *s)
+{ printf("inside proto_session_init\n");
+  if (s) bzero(s, sizeof(Proto_Session));
+}
+
+extern void
+proto_session_reset_send(Proto_Session *s)
+{ printf("inside proto_session_reset_send\n");
+  bzero(&s->shdr, sizeof(s->shdr));
+  s->slen = 0;
+}
+
+extern void
+proto_session_reset_receive(Proto_Session *s)
+{printf("inside proto_session_reset_receive\n");
+  bzero(&s->rhdr, sizeof(s->rhdr));
+  s->rlen = 0;
+}
+
+static void
+proto_session_hdr_marshall_sver(Proto_Session *s, Proto_StateVersion v)
+{
+    printf(" inside proto_session_hdr_marshall_sver\n");
+  s->shdr.sver.raw = htonll(v.raw);
+}
+
+static void
+proto_session_hdr_unmarshall_sver(Proto_Session *s, Proto_StateVersion *v)
+{
+    printf(" inside proto_session_hdr_unmarshall_sver\n");
+  v->raw = ntohll(s->rhdr.sver.raw);
+}
+
+static void
+proto_session_hdr_marshall_pstate(Proto_Session *s, Proto_Player_State *ps)
+{
+    printf(" inside proto_session_hdr_marshall_pstate\n");
+  s->shdr.pstate.v0.raw  = htonl(ps->v0.raw);
+  s->shdr.pstate.v1.raw  = htonl(ps->v1.raw);
+  s->shdr.pstate.v2.raw  = htonl(ps->v2.raw);
+  s->shdr.pstate.v3.raw  = htonl(ps->v3.raw);
+
+
+}
+
+static void
+proto_session_hdr_unmarshall_pstate(Proto_Session *s, Proto_Player_State *ps)
+{printf("inside proto_session_hdr_unmarshall_pstate\n");
+ 
+  s->rhdr.pstate.v0.raw = ntohl(ps->v0.raw);
+  s->rhdr.pstate.v0.raw = ntohl(ps->v1.raw);
+  s->rhdr.pstate.v0.raw = ntohl(ps->v2.raw);
+  s->rhdr.pstate.v0.raw = ntohl(ps->v3.raw);
+
+  //ps->v0.raw  = ntohl(s->rhdr.pstate.v0.raw);
+  //ps->v1.raw  = ntohl(s->rhdr.pstate.v1.raw);
+  //ps->v2.raw  = ntohl(s->rhdr.pstate.v2.raw);
+  //ps->v3.raw  = ntohl(s->rhdr.pstate.v3.raw);
+
+}
+
+static void
+proto_session_hdr_marshall_gstate(Proto_Session *s, Proto_Game_State *gs)
+{
+    printf(" inside proto_session_hdr_marshall_gstate\n");
+ s->shdr.gstate.v0.raw = htonl(gs->v0.raw);
+ s->shdr.gstate.v1.raw = htonl(gs->v1.raw);
+ s->shdr.gstate.v2.raw = htonl(gs->v2.raw);
+ 
+}
+
+static void
+proto_session_hdr_unmarshall_gstate(Proto_Session *s, Proto_Game_State *gs)
+{  
+    printf("inside proto_session_hdr_unmarshall_gstate\n");
+  s->rhdr.gstate.v0.raw = ntohl(gs->v0.raw);
+  s->rhdr.gstate.v1.raw = ntohl(gs->v1.raw);
+  s->rhdr.gstate.v2.raw = ntohl(gs->v2.raw);
+
+
+  //gs->v0.raw = ntohl(s->rhdr.gstate.v0.raw);
+  //gs->v1.raw = ntohl(s->rhdr.gstate.v1.raw);
+  //gs->v2.raw = ntohl(s->rhdr.gstate.v2.raw);
+}
+
+static int
+proto_session_hdr_unmarshall_blen(Proto_Session *s)
+{
+    printf(" inside proto_session_hdr_unmarshall_blen\n");
+  ntohl(s->shdr.blen);
+  return 0;
+}
+
+static void
+proto_session_hdr_marshall_type(Proto_Session *s, Proto_Msg_Types t)
+{
+    printf("inside proto_session_hdr_marshall_type\n");
+  s->shdr.type = htonl(t);
+
+}
+
+static int
+proto_session_hdr_unmarshall_version(Proto_Session *s)
+{
+  printf("inside proto_session_hdr_unmarshall_version\n");
+  ntohl(s->shdr.version);
+  return 0;
+}
+
+extern Proto_Msg_Types
+proto_session_hdr_unmarshall_type(Proto_Session *s)
+{
+    printf("inside proto_session_hdr_unmarshall_type\n");
+    return ntohl(s->rhdr.type);
+}
+
+extern void
+proto_session_hdr_unmarshall(Proto_Session *s, Proto_Msg_Hdr *h)
+{
+    printf("inside proto_session_hdr_unmarshall\n ");
+  h->version = proto_session_hdr_unmarshall_version(s);
+  h->type = proto_session_hdr_unmarshall_type(s);
+  proto_session_hdr_unmarshall_sver(s, &h->sver);
+  proto_session_hdr_unmarshall_pstate(s, &h->pstate);
+  proto_session_hdr_unmarshall_gstate(s, &h->gstate);
+  h->blen = proto_session_hdr_unmarshall_blen(s);
+}
+   
+extern void
+proto_session_hdr_marshall(Proto_Session *s, Proto_Msg_Hdr *h)
+{
+    printf("inside proto_session_hdr_marshall\n");
+  // ignore the version number and hard code to the version we support
+  s->shdr.version = PROTOCOL_BASE_VERSION;
+  proto_session_hdr_marshall_type(s, h->type);
+  proto_session_hdr_marshall_sver(s, h->sver);
+  proto_session_hdr_marshall_pstate(s, &h->pstate);
+  proto_session_hdr_marshall_gstate(s, &h->gstate);
+  // we ignore the body length as we will explicity set it
+  // on the send path to the amount of body data that was
+  // marshalled.
+}
+
+extern int 
+proto_session_body_marshall_ll(Proto_Session *s, long long v)
+{
+    printf(" inside proto_session_body_marshall_ll\n");
+  if (s && ((s->slen + sizeof(long long)) < PROTO_SESSION_BUF_SIZE)) {
+    *((int *)(s->sbuf + s->slen)) = htonll(v);
+    s->slen+=sizeof(long long);
+      printf("succesfull exit from proto_session_body_marshall_ll\n");
+    return 1;
+  }
+    printf("error proto_session_body_marshall_ll\n");
+  return -1;
+}
+
+extern int 
+proto_session_body_unmarshall_ll(Proto_Session *s, int offset, long long *v)
+{
+    printf("inside proto_session_body_unmarshall_ll\n");
+    if (s && ((s->rlen - (offset + (int)sizeof(long long))) >=0 )) {
+    *v = *((long long *)(s->rbuf + offset));
+    *v = htonl(*v);
+      printf("succesfull exit proto_session_body_unmarshall_ll\n");
+    return offset + sizeof(long long);
+  }
+    printf("error proto_session_body_unmarshall_ll\n");
+  return -1;
+}
+
+extern int 
+proto_session_body_marshall_int(Proto_Session *s, int v)
+{printf("inside proto_session_body_marshall_int\n");
+  if (s && ((s->slen + sizeof(int)) < PROTO_SESSION_BUF_SIZE)) {
+    *((int *)(s->sbuf + s->slen)) = htonl(v);
+    s->slen+=sizeof(int);
+      printf(" sucess proto_session_body_marshall_int\n");
+    return 1;
+  }
+    printf("error proto_session_body_marshall_int\n");
+  return -1;
+}
+
+extern int 
+proto_session_body_unmarshall_int(Proto_Session *s, int offset, int *v)
+{printf("inside proto_session_body_unmarshall_int\n");
+  if (s && ((s->rlen  - (offset + (int)sizeof(int))) >=0 )) {
+    *v = *((int *)(s->rbuf + offset));
+    *v = htonl(*v);
+      printf("sucess proto_session_body_unmarshall_int\n");
+    return offset + sizeof(int);
+  }
+    printf("error proto_session_body_unmarshall_int\n");
+  return -1;
+}
+
+extern int 
+proto_session_body_marshall_char(Proto_Session *s, char v)
+{printf("inside proto_session_body_marshall_char\n");
+  if (s && ((s->slen + sizeof(char)) < PROTO_SESSION_BUF_SIZE)) {
+    s->sbuf[s->slen] = v;
+    s->slen+=sizeof(char);
+      printf("sucess proto_session_body_marshall_char\n");
+    return 1;
+  }
+    printf("error proto_session_body_marshall_char\n");
+  return -1;
+}
+
+extern int 
+proto_session_body_unmarshall_char(Proto_Session *s, int offset, char *v)
+{printf("inside proto_session_body_unmarshall_char\n");
+  if (s && ((s->rlen - (offset + (int)sizeof(char))) >= 0)) {
+    *v = s->rbuf[offset];
+      printf("sucess proto_session_body_unmarshall_char\n");
+    return offset + sizeof(char);
+  }
+    printf("error proto_session_body_unmarshall_char\n");
+  return -1;
+}
+
+extern int
+proto_session_body_reserve_space(Proto_Session *s, int num, char **space)
+{printf("inside proto_session_body_reserve_space\n");
+  if (s && ((s->slen + num) < PROTO_SESSION_BUF_SIZE)) {
+    *space = &(s->sbuf[s->slen]);
+    s->slen += num;
+      printf("sucess proto_session_body_reserve_space\n");
+    return 1;
+  }
+  *space = NULL;
+    printf("error proto_session_body_reserve_space\n");
+  return -1;
+}
+
+extern int
+proto_session_body_ptr(Proto_Session *s, int offset, char **ptr)
+{ printf("inside proto_session_body_ptr\n");
+  if (s && ((s->rlen - offset) > 0)) {
+    *ptr = &(s->rbuf[offset]);
+      printf("success proto_session_body_ptr\n");
+      return 1;
+  }
+    printf("error proto_session_body_ptr\n");
+  return -1;
+}
+	    
+extern int
+proto_session_body_marshall_bytes(Proto_Session *s, int len, char *data)
+{ printf("inside proto_session_body_marshall_bytes\n");
+  if (s && ((s->slen + len) < PROTO_SESSION_BUF_SIZE)) {
+    memcpy(s->sbuf + s->slen, data, len);
+    s->slen += len;
+      printf("sucess proto_session_body_marshall_bytes\n");
+    return 1;
+  }
+    printf("error proto_session_body_marshall_bytes\n");
+  return -1;
+}
+
+extern int
+proto_session_body_unmarshall_bytes(Proto_Session *s, int offset, int len, 
+				     char *data)
+{printf("inside proto_session_body_unmarshall_bytes\n");
+  if (s && ((s->rlen - (offset + len)) >= 0)) {
+    memcpy(data, s->rbuf + offset, len);
+      printf("sucess proto_session_body_unmarshall_bytes\n");
+    return offset + len;
+  }
+    printf("error proto_session_body_unmarshall_bytes\n");
+  return -1;
+}
+
+
+// rc < 0 on comm failures
+// rc == 1 indicates comm success
+extern  int
+proto_session_send_msg(Proto_Session *s, int reset)
+{
+  s->shdr.blen = htonl(s->slen);
+
+  // write request
+  if (net_writen(s->fd, &(s->shdr), sizeof(s->shdr)) != sizeof(s->shdr)) 
+    return -1;
+
+  if (s->slen) {
+    if (net_writen(s->fd, s->sbuf, s->slen) != s->slen) {
+      return -2;
+    }
+  }
+
+  if (proto_debug()) {
+    fprintf(stderr, "%p: proto_session_send_msg: SENT:\n", pthread_self());
+    proto_session_dump(s);
+  }
+
+  // communication was successfull 
+  if (reset) proto_session_reset_send(s);
+
+  return 1;
+}
+
+extern int
+proto_session_rcv_msg(Proto_Session *s)
+{
+  
+  proto_session_reset_receive(s);
+
+  // read reply
+  if (net_readn(s->fd, &(s->rhdr), sizeof(s->rhdr)) != sizeof(s->rhdr)) 
+    return -3;
+
+  s->rlen = ntohl(s->rhdr.blen);
+  if (s->rlen) {
+    if (s->rlen > PROTO_SESSION_BUF_SIZE || 
+	net_readn(s->fd, s->rbuf, s->rlen)!=s->rlen) {
+      return -4;
+    }
+  } 
+  if (proto_debug()) {
+    fprintf(stderr, "%p: proto_session_rcv_msg: RCVED:\n", pthread_self());
+    proto_session_dump(s);
+  }
+  return 1;
+}
+
+extern int
+proto_session_rpc(Proto_Session *s)
+{
+  int rc;
+  
+  rc = proto_session_send_msg(s,1);
+  if (rc==1) {
+    rc = proto_session_rcv_msg(s);
+  }
+  return rc;
+}
+
+
